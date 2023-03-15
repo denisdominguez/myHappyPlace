9f12d30fbe3e6a0e7783581c4f5191f437e669cb
denisdominguez <it@denis-dominguez.de>
2023-03-15 23:57:01 +0100

    Update HomeController. Index.mustache updated. Logo inserted Tasks file to track TODOS created.

---
+++ Tasks.md
@@ -0,0 +1,6 @@
+# TODOS
+## Denis
+1. Create new Controllers to display data
+
+## Jorgito
+1. Create the H2 database on memory configuration
--- src/main/java/com/dominguez/myHappyPlace/controllers/HomeController.java
+++ src/main/java/com/dominguez/myHappyPlace/controllers/HomeController.java
@@ -1,13 +1,24 @@
 package com.dominguez.myHappyPlace.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    record User (String name) {}
+
+    List<User> users = List.of(new User("Denis"), new User( "Jorge"), new User("Daniela"));
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("usuario", "Jorge");
+        model.addAttribute("users", users);
         return "index";
     }
 }
